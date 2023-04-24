2dfc151b49f64809b7497eed8673a0e7ce78a358
SeDe-cmd <105421555+SeDe-cmd@users.noreply.github.com>
2023-04-24 10:32:57 +0200

    Main c
    Lade till main filen för projektet

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * projekt.c
+ *
+ * Created: 2023-04-24 10:30:39
+ * Author : se8301ed-s
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
