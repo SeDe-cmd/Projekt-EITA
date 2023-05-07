eb141b60507d42d2451da0f30410186d8cc6dce2
SeDe-cmd <105421555+SeDe-cmd@users.noreply.github.com>
2023-05-07 14:59:11 +0200

    Update main.c
    small bit of trolling

--- main.c
+++ main.c
@@ -81,26 +81,26 @@ void screen2(){
 }
 uint8_t EEPROM_read(uint16_t uiAddress)
 {
- /* Wait for completion of previous write /
+ /* Wait for completion of previous write */
  while(EECR & (1<<EEPE))
  ;
- / Set up address register /
+ /* Set up address register */
  EEAR = uiAddress;
- / Start eeprom read by writing EERE /
+ /* Start eeprom read by writing EERE */
  EECR |= (1<<EERE);
- / Return data from Data Register */
+ /* Return data from Data Register */
  return EEDR;
 }
 void EEPROM_write(uint16_t uiAddress, uint8_t ucData)
 {
- /* Wait for completion of previous write /
+ /* Wait for completion of previous write */
  while(EECR & (1<<EEPE));
- / Set up address and Data Registers /
+ /* Set up address and Data Registers */
  EEAR = uiAddress;
  EEDR = ucData;
- / Write logical one to EEMPE /
+ /* Write logical one to EEMPE */
  EECR |= (1<<EEMPE);
- / Start eeprom write by setting EEPE */
+ /* Start eeprom write by setting EEPE */
  EECR |= (1<<EEPE);
 }
 void GLCD_ClearAll()			/* GLCD all display clear function */
